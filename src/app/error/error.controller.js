bcdf9126c160ee9c69069c580f4461ac7a998ddd
Gabriel Ferraz <gferraz@fullsail.com>
2017-05-15 15:43:28 -0400

    implements error page design

--- src/app/error/error.controller.js
+++ src/app/error/error.controller.js
@@ -5,8 +5,19 @@
     .module('error')
     .controller('ErrorCtrl', ErrorCtrl);
 
-  /* @ngInject */
   function ErrorCtrl() {
-  }
+    let circleOptions = {
+      value: 1,
+      size: 145,
+      thickness: 10,
+      fill: {
+        gradient: ['#008CBA', '#D4B193', '#E7FCFC', '#29704D']
+      }, 
+      animation: {
+        duration: 1800
+      }
+    };
 
+    $('.error_circle').circleProgress(circleOptions);
+  }
 })();
